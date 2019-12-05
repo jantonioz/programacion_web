97bae114fcc7e23fef91b1021cc4f3df1dbd490a
Marcos Saucedo <marcos100299@gmail.com>
2019-12-04 20:05:17 -0600

    marGOD has arrived

---
+++ precache-manifest.00c1819f4b412847d00931f0b9b3eb90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6a53964af8bcdfea39524f25ccd3805",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e47fe24e1abb82095f5c",
+    "url": "/static/css/main.008bd0b9.chunk.css"
+  },
+  {
+    "revision": "dba842bf46ad37e3c05e",
+    "url": "/static/js/2.3d7ad7ad.chunk.js"
+  },
+  {
+    "revision": "e47fe24e1abb82095f5c",
+    "url": "/static/js/main.3b5d0c71.chunk.js"
+  },
+  {
+    "revision": "f649b18296e62814dce0",
+    "url": "/static/js/runtime-main.2c7d9cb5.js"
+  }
+]);
---
+++ precache-manifest.0a62faf82f29af6be195e8fe4b41997b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30582651904a7e993879df29524a53b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ecd19bd9fc02025cee0",
+    "url": "/static/css/main.d95a9886.chunk.css"
+  },
+  {
+    "revision": "dba842bf46ad37e3c05e",
+    "url": "/static/js/2.3d7ad7ad.chunk.js"
+  },
+  {
+    "revision": "6ecd19bd9fc02025cee0",
+    "url": "/static/js/main.a1ddac36.chunk.js"
+  },
+  {
+    "revision": "f649b18296e62814dce0",
+    "url": "/static/js/runtime-main.2c7d9cb5.js"
+  }
+]);
